38eb57ac8a06311b74e875ef28b3a69cbeda5ad5
crustyanand <aa9004@srmist.edu.in>
2026-03-11 15:10:09 +0530

    Use Case 6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,33 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
+
+        String input = "level";
+
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            stack.push(c);
+            queue.add(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (!stack.isEmpty() && !queue.isEmpty()) {
+            if (stack.pop() != queue.remove()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
 
     }
 }
