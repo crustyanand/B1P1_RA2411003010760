04a5b3df7117805f0764e45acc0bb032c487383c
crustyanand <aa9004@srmist.edu.in>
2026-03-11 14:53:19 +0530

    Use case 3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,21 @@
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
+
+        String original = "madam";
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println("Input text: " + original);
+            System.out.println("Is it a Palindrome? : true");
+        } else {
+            System.out.println("Input text: " + original);
+            System.out.println("Is it a Palindrome? : false");
+        }
 
     }
 }
